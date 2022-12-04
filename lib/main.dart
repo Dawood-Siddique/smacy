466657c1a0fb1b401d71a98485823a1115dcd321
Dawood-Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2022-12-04 20:53:08 +0500

    Added picture also added picture in pubspec.yaml made some changes in main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: GetStarted(),
+    MaterialApp(
+      home: const GetStarted(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromRGBO(11, 26, 13, 1),
+        primaryColor: const Color.fromRGBO(173, 225,0 , 1),
+      ),
     )
   );
 }
+// 173,225,0 as main color
+// 11,26,13 as background color
 
 
 class GetStarted extends StatelessWidget {
@@ -14,6 +20,12 @@ class GetStarted extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: const [
+          Center(child: Text('Hello there')),
+        ],
+      ),
+    );
   }
 }
